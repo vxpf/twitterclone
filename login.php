7fcf5ac6ba0bcdfba6dd5ad49d8732bb0290d2d6
Marvellous010 <9025915@student.zadkine.nl>
2025-02-19 08:42:37 +0100

    Nieuw

--- login.php
+++ login.php
@@ -3,19 +3,20 @@
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <script
-            src="https://kit.fontawesome.com/64d58efce2.js"
-            crossorigin="anonymous"
-    ></script>
+    <link
+            rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
+    />
+
     <link rel="stylesheet" href="style.css" />
-    <title>Sign in / Sign up Form</title>
+    <title>Login in</title>
 </head>
 <body>
 <div class="container">
     <div class="forms-container">
         <div class="signin-signup">
             <form action="#" class="sign-in-form">
-                <h2 class="title">Sign in</h2>
+                <h2 class="title">Log in</h2>
                 <div class="input-field">
                     <i class="fas fa-user"></i>
                     <input type="text" placeholder="Username" />
@@ -25,24 +26,9 @@
                     <input type="password" placeholder="Password" />
                 </div>
                 <input type="submit" value="Login" class="btn solid" />
-                <p class="social-text">Or Sign in with social platforms</p>
-                <div class="social-media">
-                    <a href="#" class="social-icon">
-                        <i class="fab fa-facebook-f"></i>
-                    </a>
-                    <a href="#" class="social-icon">
-                        <i class="fab fa-twitter"></i>
-                    </a>
-                    <a href="#" class="social-icon">
-                        <i class="fab fa-google"></i>
-                    </a>
-                    <a href="#" class="social-icon">
-                        <i class="fab fa-linkedin-in"></i>
-                    </a>
-                </div>
             </form>
             <form action="#" class="sign-up-form">
-                <h2 class="title">Sign up</h2>
+                <h2 class="title">Registreer</h2>
                 <div class="input-field">
                     <i class="fas fa-user"></i>
                     <input type="text" placeholder="Username" />
@@ -56,21 +42,6 @@
                     <input type="password" placeholder="Password" />
                 </div>
                 <input type="submit" class="btn" value="Sign up" />
-                <p class="social-text">Or Sign up with social platforms</p>
-                <div class="social-media">
-                    <a href="#" class="social-icon">
-                        <i class="fab fa-facebook-f"></i>
-                    </a>
-                    <a href="#" class="social-icon">
-                        <i class="fab fa-twitter"></i>
-                    </a>
-                    <a href="#" class="social-icon">
-                        <i class="fab fa-google"></i>
-                    </a>
-                    <a href="#" class="social-icon">
-                        <i class="fab fa-linkedin-in"></i>
-                    </a>
-                </div>
             </form>
         </div>
     </div>
@@ -78,10 +49,9 @@
     <div class="panels-container">
         <div class="panel left-panel">
             <div class="content">
-                <h3>New here ?</h3>
+                <h3>Nieuw hier?</h3>
                 <p>
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Debitis,
-                    ex ratione. Aliquid!
+                    Maak dan een nieuw geweldige account naar deze mooie site!
                 </p>
                 <button class="btn transparent" id="sign-up-btn">
                     Sign up
@@ -91,13 +61,12 @@
         </div>
         <div class="panel right-panel">
             <div class="content">
-                <h3>One of us ?</h3>
+                <h3>Ben je een van ons?</h3>
                 <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
-                    laboriosam ad deleniti.
+                    Log dan in!
                 </p>
                 <button class="btn transparent" id="sign-in-btn">
-                    Sign in
+                   Log in
                 </button>
             </div>
             <img src="reg.svg" class="image" alt="" />
