fad83c3a5d2de8e99acbeae8c3dc92160d8cc65e
Marvellous010 <9025915@student.zadkine.nl>
2025-02-19 10:43:31 +0100

    Nieuw 2

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login / registreer</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+<div class="container">
+    <div class="form-box" id="login-form">
+        <form action="">
+            <h2>Login</h2>
+            <input type="email" name="email" placeholder="Email" required>
+                <input type="password" name="password" placeholder="Password" required>
+                <button type="submit" name="login">Login</button>
+                <p>Don't have an account? <a href="#">Register</a></p>
+        </form>
+    </div>
+</div>
+
+</body>
+
+</html>
