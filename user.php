ed12b4bc870ecbee32815132ec35907e6f083ff9
Marvellous010 <9025915@student.zadkine.nl>
2025-03-04 14:01:34 +0100

    Veranderingen 5

--- user.php
+++ user.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-if (!isset($_SESSION['role']) || $_SESSION['role'] != 'Admin') {
+if (!isset($_SESSION['role']) || $_SESSION['role'] != 'User') {
     header("Location: index.php");
     exit();
 }
